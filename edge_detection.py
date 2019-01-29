37059a18c46d6a75ba4586e4ee0ea10b33fc81c8
andres <andresulloa97@gmail.com>
2019-01-29 12:00:39 -0600

    first commit

---
+++ edge_detection.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+import cv2 as cv
+
+
+#0 for vertical and 1 for horizontal
+def sobelFiltering(img, direction):
+    pass
+
+
+#0 for vertical and 1 for horizontal
+def laplaceFiltering(img, direction):
+    pass
+
+
+#0 for vertical and 1 for horizontal
+def edgeDetection(img, direction, threshold):
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
