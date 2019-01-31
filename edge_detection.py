4b1ec0ffaa821e22d055e36d4e6b2b4dd668af97
andres <andresulloa97@gmail.com>
2019-01-31 11:23:54 -0600

    computes linear combination between window and kernels

--- edge_detection.py
+++ edge_detection.py
@@ -3,14 +3,54 @@ import numpy as np
 import cv2 as cv
 
 
+def convolution(image, kernel):
+
+    filtered_image = image
+    num_rows = image.shape[1]
+    num_cols = image.shape[0]
+
+    kernel_size = (kernel.shape[0], kernel.shape[1])
+
+    #asumes the kernel is simmetric and of odd dimensions
+    edge = int(math.floor(kernel.shape[0]/2))
+
+    for i in range(edge, num_rows - edge):
+        for j in range(edge, num_cols - edge):
+            window = fill_window(i,j, kernel_size, image)
+            image[i,j] = get_linear_combination(window, kernel)
+            
+
+    return filtered_image
+
+
+def fill_window(kernel_mid_x, kernel_mid_y, kernel_size, image):
+    
+    kernel = np.zeros((kernel_size[0], kernel_size[1]), dtype = int)
+    from_x = kernel_mid_x - int(math.floor(kernel_size[1]/2))
+    to_x = kernel_mid_x + int(math.floor(kernel_size[1]/2))
+    from_y = kernel_mid_y -  int(math.floor(kernel_size[0]/2))
+    to_y = kernel_mid_y +  int(math.floor(kernel_size[0]/2))
+
+    for i in range(from_x, to_x + 1):
+        for j in range(from_y, to_y + 1):
+
+            kernel[i - from_x, j - from_y] = image[i,j]
+            
+    return kernel
+
+
+def get_linear_combination(matrixA, matrixB):
+    pass
+
 #1 for vertical and 0 for horizontal
 def sobelFiltering(img, direction):
-    pass
+    sobel_filter_x = np.matrix('-1,0,1;-2,0,2;-1,0,1')
+    sobel_filter_y = np.matrix('-1,-2,-1;0,0,0;1,2,1')
 
 
-#1 for vertical and 0 for horizontal
-def laplaceFiltering(img, direction):
-    pass
+def laplacianFiltering(img):
+    laplacian_filter_x =  np.matrix('0,-1,0;-1,4,-1;0,-1,0')
+    
 
 
 #1 for vertical and 0 for horizontal
@@ -21,6 +61,6 @@ def edgeDetection(img, direction, threshold):
 def main():
     img = cv.imread('lena-grey.png')
     cv.imwrite(sobelFiltering(img, 0), 'sobel-x.png')
-    
+
 if __name__ == '__main__':
     main()
