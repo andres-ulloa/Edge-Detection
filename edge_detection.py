ccbd7c5d057e3b69653478258a59ddedc2cc0b0e
andres <andresulloa97@gmail.com>
2019-02-03 10:06:25 -0600

    done

--- edge_detection.py
+++ edge_detection.py
@@ -5,7 +5,7 @@ import math
 
 def convolution(image, kernel):
 
-    filtered_image = image
+    filtered_image =  np.zeros((image.shape[0], image.shape[1]), dtype = float)
     num_rows = image.shape[1]
     num_cols = image.shape[0]
 
@@ -16,8 +16,11 @@ def convolution(image, kernel):
 
     for i in range(edge, num_rows - edge):
         for j in range(edge, num_cols - edge):
+
             window = fill_window(i,j, kernel_size, image)
-            image[i,j] = get_linear_combination(window, kernel)
+            dot_product = get_linear_combination(window, kernel)
+            filtered_image[i,j] = dot_product
+            #print('INSIDE = ' , filtered_image[i,j])
             
 
     return filtered_image
@@ -25,7 +28,7 @@ def convolution(image, kernel):
 
 def fill_window(kernel_mid_x, kernel_mid_y, kernel_size, image):
     
-    kernel = np.zeros((kernel_size[0], kernel_size[1]), dtype = int)
+    window = np.zeros((kernel_size[0], kernel_size[1]), dtype = int)
     from_x = kernel_mid_x - int(math.floor(kernel_size[1]/2))
     to_x = kernel_mid_x + int(math.floor(kernel_size[1]/2))
     from_y = kernel_mid_y -  int(math.floor(kernel_size[0]/2))
@@ -33,22 +36,23 @@ def fill_window(kernel_mid_x, kernel_mid_y, kernel_size, image):
 
     for i in range(from_x, to_x + 1):
         for j in range(from_y, to_y + 1):
-
-            kernel[i - from_x, j - from_y] = image[i,j]
+            window[i - from_x, j - from_y] = image[i,j]
             
-    return kernel
+    return window
 
 
 def get_linear_combination(matrixA, matrixB):
     
-    rows = matrixA.shape[0]
-    cols = matrixA.shape[1]
+    rows = matrixA.shape[1]
+    cols = matrixA.shape[0]
     linear_comb = 0
 
     for i in range(0, rows):
         for j in range(0, cols):
             linear_comb += matrixA[i,j] * matrixB[i,j]
-
+            """print(matrixA[i,j], ' * ', matrixB[i,j], ' = ' ,matrixA[i,j] * matrixB[i,j])
+            print('\nLC so far = ', linear_comb)"""
+    
     return linear_comb
 
 
@@ -98,14 +102,16 @@ def prewittFiltering(img, direction):
 
 
 def laplacianFiltering(img):
-    laplacian_filter_x =  np.matrix('0,-1,0;-1,4,-1;0,-1,0')
-    
+    laplacian_filter =  np.matrix('0,-1,0;-1,4,-1;0,-1,0')
+    second_derivative = convolution(img, laplacian_filter)
+    return second_derivative
+
 
 def compute_edge_magnitude(gradients_x, gradients_y):
     gradient_magnitude = np.zeros((gradients_x.shape[0], gradients_x.shape[1]), dtype = float)
     for i in range(0, gradients_x.shape[0]):
         for j in range(0, gradients_y.shape[1]):
-            gradient_magnitude[i,j] = abs(gradients_x[i,j]) + abs(gradients_y[i,j]) 
+            gradient_magnitude[i,j] = math.sqrt(pow(gradients_x[i,j], 2) + pow(gradients_y[i,j],2))
 
     return gradient_magnitude
 
@@ -114,9 +120,11 @@ def compute_edge_direction(gradients_x, gradients_y):
     gradient_direction = np.zeros((gradients_x.shape[0], gradients_x.shape[1]), dtype = float)
     for i in range(0, gradients_x.shape[0]):
         for j in range(0, gradients_y.shape[1]):
-            gradient_direction[i,j] = math.atan(gradients_x[i,j] / gradients_y[i,j])
+            if gradients_y[i,j] != 0:
+                gradient_direction[i,j] = math.atan(gradients_x[i,j] / gradients_y[i,j])
 
     return gradient_direction
+
 
 def umbralize(img, thresh):
 
@@ -129,6 +137,8 @@ def umbralize(img, thresh):
             else:
                 umbralized_img[i,j] = 255
 
+    return umbralized_img
+    
 
 #1 for vertical and 0 for horizontal
 def edgeDetection(img, type_, threshold = 150):
@@ -150,24 +160,28 @@ def edgeDetection(img, type_, threshold = 150):
         print('nop')
         exit(0)
 
-    cv.imwrite(gradients_x, 'edge_detection_x.png')
-    cv.imwrite(gradients_y, 'edge_detection_y.png')
+    cv.imwrite('edge_detection_x.png', gradients_x)
+    cv.imwrite( 'edge_detection_y.png', gradients_y)
 
     edge_magnitude = compute_edge_magnitude(gradients_x, gradients_y)
     edge_orientation = compute_edge_direction(gradients_x, gradients_y)
-    cv.imwrite(edge_magnitude, 'pre_umbralized_mag.png')
+    cv.imwrite('pre_umbralized_mag.png' , edge_magnitude)
 
-    umbralized_mag = umbralize(edge_magnitude, threshold)
-    cv.imwrite(umbralized_mag, 'umbralized_mag.png')
-    
+    return umbralize(edge_magnitude, threshold)
+   
+
+
 
 
 def main():
 
-    img = cv.imread('lena-grey.png')
-    cv.imwrite(sobelFiltering(img, 0), 'sobel-x.png')
-    cv.imwrite(sobelFiltering(img,1),  'sobel-y.png')
-
+    img = cv.imread('lena_grey.png',0)
+    """cv.imwrite('sobel-x.png', sobelFiltering(img, 0))
+    cv.imwrite( 'sobel-y.png', sobelFiltering(img,1))
+    cv.imwrite('laplacian.png', laplacianFiltering(img))
+    """
+    umbralized_mags = edgeDetection(img, 'prewitt', 100)
+    cv.imwrite('umbralized_mag.png', umbralized_mags)
 
 if __name__ == '__main__':
     main()
